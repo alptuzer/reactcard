e57baf1b135a7255ad793bc69bb5b61f342a4419
alp <alptuzer@gmail.com>
2024-03-27 18:58:57 +0300

    main

---
+++ reactcard/src/components/Card.js
@@ -0,0 +1,22 @@
+import React from 'react'
+// rfc
+import '../styles/Card.css'
+
+function Card(props) {
+  return (
+    <div>
+      <div className='card'>
+        <div className='cardBody'>
+        <img src={props.img} />
+            <div className='about'>
+            <h2 className='cardTitle'>{props.title}</h2>
+          <p className='cardDescription'>{props.description}</p>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+export default Card;
